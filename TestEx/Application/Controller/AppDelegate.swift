10ec49b76558b6eb7605ded0216c9bf653dadcce
Alexandr <alexandrshubskij@gmail.com>
2020-10-07 13:36:47 +0300

    Initial commit with MVC

---
+++ TestEx/Application/Controller/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  TestEx
+//
+//  Created by Александр Шубский on 07.10.2020.
+//  Copyright © 2020 Александр Шубский. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+    
+
+    
+
+    
+
+
+}
+
